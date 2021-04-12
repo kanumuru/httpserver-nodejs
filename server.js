3c529e3a598969ae274e0ea9c5f28a1acb1fd417
Rajesh Reddy <kanumururajeshreddy@gmail.com>
2021-04-12 11:27:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('The port 8080 is enabled now on Security Group!');
+  res.end('Welcome to AppsecEnginer Training! The port 8080 is enabled now on Security Group!');
 }
 
 const server = http.createServer(requestListener);
